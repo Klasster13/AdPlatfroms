08177e91ddb2ed1d6146bda9cd6547a2860c76fc
kristieh <kristieh@student.21-school.ru>
2025-09-21 22:34:26 +0500

    Add: logger for repository

--- AdPlatforms/Repository/Impl/DataRepository.cs
+++ AdPlatforms/Repository/Impl/DataRepository.cs
@@ -1,10 +1,12 @@
 ﻿using AdPlatforms.Models;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 
 namespace AdPlatforms.Repository.Impl;
 
-public class DataRepository : IDataRepository
+public class DataRepository(ILogger<IDataRepository> logger) : IDataRepository
 {
+    private readonly ILogger<IDataRepository> _logger = logger;
     private LocationTreeNode _root = new();
     private readonly ConcurrentDictionary<string, List<string>> _cache = [];
 
@@ -15,6 +17,10 @@ public class DataRepository : IDataRepository
 
         var newRoot = new LocationTreeNode();
 
+        _logger.LogDebug("Starting creating data trie. Platforms count: {platformCount}.",
+            platforms.Count);
+        var stopwatch = Stopwatch.StartNew();
+
         foreach (var platform in platforms)
         {
             foreach (var location in platform.Locations)
@@ -22,9 +28,17 @@ public class DataRepository : IDataRepository
                 AddPlatformToTrie(newRoot, location, platform.Name);
             }
         }
+        stopwatch.Stop();
+
+        _logger.LogDebug("Finished creating data trie. Elapsed time: {time}ms.",
+            stopwatch.ElapsedMilliseconds);
 
         _root = newRoot;
+        var cacheSizeDeleted = _cache.Count;
         _cache.Clear();
+
+        _logger.LogDebug("Cleared cache size: {size}.",
+            cacheSizeDeleted);
     }
 
 
@@ -58,25 +72,32 @@ public class DataRepository : IDataRepository
 
     public List<string> FindPlatromsForLocation(string targetLocation)
     {
+        _logger.LogDebug("Searching for location: {targetLocation}.",
+            targetLocation);
+
         if (string.IsNullOrWhiteSpace(targetLocation))
         {
+            _logger.LogWarning("Empty target location provided for search.");
             return [];
         }
 
-        // Trying to find data saved in cache
         if (_cache.TryGetValue(targetLocation, out var cachedResult))
         {
+            _logger.LogDebug("Cache found for location: {targetLocation}.",
+                targetLocation);
             return cachedResult;
         }
+
+        _logger.LogDebug("No cache found for location: {targetLocation}.",
+                targetLocation);
 
         var parts = targetLocation.Split('/', StringSplitOptions.RemoveEmptyEntries);
         var currentNode = _root;
         var tmpData = new HashSet<string>();
+        var stopwatch = Stopwatch.StartNew();
 
-        // Searchin data in trie
         foreach (var part in parts)
         {
-            // Checking if current location part is existing in current node children
             if (!currentNode.Children.TryGetValue(part, out currentNode))
             {
                 break;
@@ -85,9 +106,11 @@ public class DataRepository : IDataRepository
         }
 
         var resultData = tmpData.ToList();
-
-        // Caching result
+        stopwatch.Stop();
         _cache[targetLocation] = resultData;
+
+        _logger.LogDebug("Search coplete. Location: {targetLocation}. Elapsed time {time}ms.",
+                targetLocation, stopwatch.ElapsedMilliseconds);
 
         return resultData;
     }
